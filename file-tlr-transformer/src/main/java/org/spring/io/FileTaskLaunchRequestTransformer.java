046f008a3b1d2a010a36a93564b81377f37026b1
David Turanski <dturanski@pivotal.io>
2017-04-20 15:26:22 -0400

    Add file-tlr-transformer

---
+++ file-tlr-transformer/src/main/java/org/spring/io/FileTaskLaunchRequestTransformer.java
@@ -0,0 +1,56 @@
+/*
+ *
+ *   Copyright 2017 the original author or authors.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *
+ */
+
+package org.spring.io;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.integration.annotation.Transformer;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author David Turanski
+ **/
+@EnableBinding(Processor.class)
+@EnableConfigurationProperties(FileTlrProperties.class)
+public class FileTaskLaunchRequestTransformer {
+
+	@Autowired
+	private FileTlrProperties properties;
+
+	@Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+	public SimpleTaskLaunchRequest supplyFileName(String payload) {
+
+		return new SimpleTaskLaunchRequest
+				(properties.getTaskName(), properties.getArgs(), Collections.singletonMap
+						("fileName",
+						payload));
+	}
+
+	@Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+	public SimpleTaskLaunchRequest supplyProperties(Map<String,String> payload) {
+		return new SimpleTaskLaunchRequest
+				(properties.getTaskName(), properties.getArgs(),payload);
+	}
+
+}
