8372df066ddd3deafa3728ec6d51f079e7dd6cb1
David Turanski <dturanski@pivotal.io>
2017-04-21 00:07:29 -0400

    Start of Example

--- file-tlr-transformer/src/test/java/org/spring/io/FileTlrTransformerApplicationTests.java
+++ file-tlr-transformer/src/test/java/org/spring/io/FileTlrTransformerApplicationTests.java
@@ -29,7 +29,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest("taskName=foo")
+@SpringBootTest
 public class FileTlrTransformerApplicationTests {
 
 	@Autowired
